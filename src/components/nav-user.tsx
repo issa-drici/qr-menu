2f31aa9d4609689f41567e31be82c4ec6f6ca882
Issa Drici <issadrici@Issas-MacBook-Air.local>
2023-12-14 16:31:31 +0100

    nav bar

---
+++ src/components/nav-user.tsx
@@ -0,0 +1,60 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useUserContext } from "@/context/user";
+import Logout from "@/pages/logout";
+import { ExitIcon, ExternalLinkIcon } from "@radix-ui/react-icons";
+
+
+
+export const NavUser = () => {
+  const { logout } = useUserContext();
+
+  
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="relative h-8 w-8 rounded-full p-0 drop-shadow-lg ml-5" 
+        >
+          <Avatar className="h-8 w-8">
+            <AvatarImage
+              src="https://www.w3schools.com/w3images/avatar2.png"
+              alt="@shadcn"
+            />
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">Restaurant Del Arte</p>
+            {/* <p className="text-sm font-medium leading-none">{`${user?.prenom} ${user?.nom}`}</p> */}
+            <p className="text-xs leading-none text-muted-foreground">
+              info@delarte.com
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          Gérer mon abonnement
+          <DropdownMenuShortcut><ExternalLinkIcon /></DropdownMenuShortcut>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => Logout()}>
+          Se déconnecter
+          <DropdownMenuShortcut><ExitIcon /></DropdownMenuShortcut>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,41 +1,48 @@
 import Link from "next/link";
 import { useUserContext } from "@/context/user";
+import { useRouter } from "next/router";
+import { NavUser } from "./nav-user";
 
 const Nav = () => {
     const { user } = useUserContext();
+    const router = useRouter();
+
+    const isActive = (pathname) => router.pathname === pathname;
 
     const restaurantId = 'issddfsdf-2332432df-32dfgsdzdqds'
 
     return (
-        <nav className="flex py-4 px-6 border-b border-gray-200">
-            {/* <Link href="/">
-                Home
-            </Link> */}
-            {!!user && (
-                <Link href={`/restaurant/${restaurantId}/admin/restaurant`} legacyBehavior>
-                    <a className="ml-2">Restaurant</a>
-                </Link>
-            )}
+        <nav className="bg-gray-800 p-2 mt-0 w-full">
+            <div className="container mx-auto flex flex-wrap items-center">
+                <div className="flex w-1/2 justify-start text-white font-extrabold">
+                    <a href="#" className="text-white no-underline hover:text-white hover:no-underline">
+                        <span className="text-2xl pl-2"><i className="em em-grinning"></i>Qr Menu</span>
+                    </a>
+                </div>
+                <div className="flex w-1/2 justify-end content-center">
+                    {!!user && (
+                        <Link href={`/restaurant/${restaurantId}/admin/restaurant`} legacyBehavior>
+                            <a className={isActive(`/restaurant/[restaurantId]/admin/restaurant`) ? "text-white px-3 py-2 rounded-md text-sm font-medium": "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"}>Restaurant</a>
+                        </Link>
+                    )}
 
-            <Link href={`/restaurant/${restaurantId}/admin/restaurant`} legacyBehavior>
-                <a className="ml-2">Restaurant</a>
-            </Link>
+                    <Link href={`/restaurant/${restaurantId}/admin/restaurant`} legacyBehavior>
+                        <a className={isActive(`/restaurant/[restaurantId]/admin/restaurant`) ? "text-white px-3 py-2 rounded-md text-sm font-medium": "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"}>Restaurant</a>
+                    </Link>
 
-            <Link href={`/restaurant/${restaurantId}/admin/category`} legacyBehavior>
-                <a className="ml-2">Catégorie</a>
-            </Link>
+                    <Link href={`/restaurant/${restaurantId}/admin/category`} legacyBehavior>
+                        <a className={isActive(`/restaurant/[restaurantId]/admin/category`) ? "text-white px-3 py-2 rounded-md text-sm font-medium": "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"}>Catégorie</a>
+                    </Link>
 
-            <Link href={`/restaurant/${restaurantId}/admin/items`} legacyBehavior>
-                <a className="ml-2">Items</a>
-            </Link>
-
-            {/*  <Link href={`/restaurant/${restaurantId}/admin/menu`} legacyBehavior>
-                <a className="ml-2">Menu</a>
-            </Link>
-            <Link href={user ? "/logout" : "/login"} legacyBehavior>
-                <a className="ml-auto">{user ? "Logout" : "Login"}</a>
-            </Link> */}
+                    <Link href={`/restaurant/${restaurantId}/admin/items`} legacyBehavior>
+                        <a className={isActive(`/restaurant/[restaurantId]/admin/items`) ? "text-white px-3 py-2 rounded-md text-sm font-medium": "text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"}>Items</a>
+                    </Link>
+                    <NavUser />
+                </div>
+            </div>
         </nav>
+
+
     );
 };
 
